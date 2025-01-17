3dc60442f20d629d0ed9e7c6192e227ea4445b28
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2025-01-17 23:51:30 +0100

    add loading to share meal form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,8 @@
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+
 import classes from "./page.module.css";
+
 import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
@@ -42,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
